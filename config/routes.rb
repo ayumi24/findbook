3ca8a6fa26fdf79961c41de3a693ae416ac62c6a
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2023-01-25 04:04:24 +0000

    [Add]viewファイル作成、ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  devise_for :admin, controllers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :user, controllers: {
+    registrations: "user/registrations",
+    sessions: "user/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
