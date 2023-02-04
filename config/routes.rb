ac0593ab0e4bc963f52b82868d7f265841077423
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2023-02-04 06:38:23 +0000

    [Add]管理者TOPページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
     registrations: "user/registrations",
     sessions: "user/sessions"
   }
+
+  namespace :admin do
+    get 'homes/top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(email: "ayumisasaki1124@gmail.com",
+              password: "ayumi1124")
