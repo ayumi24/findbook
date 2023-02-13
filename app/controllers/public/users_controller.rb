fc4800b1af3ac0346519fa90565cbc2dedc5efe6
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2023-02-13 10:52:04 +0000

    [Add]会員情報登録、編集画面作成中

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,14 +3,25 @@ class Public::UsersController < ApplicationController
   end
 
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    user = current_user
+    user.update(user_params)
+    redirect_to public_user_path(user.id)
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image_url, :email)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          authentication_keys: [:name] #ユーザーログインでnameカラムを許可する
   has_many :posts, dependent: :destroy
+  has_one_attached :image
+
+  def get_image(width,height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image.variant(resize_to_limit: [width, height]).processed
+  end
 end
