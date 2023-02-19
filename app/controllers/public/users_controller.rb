a18adf403d15ac8136efab9035034a7e16d81d5c
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2023-02-19 08:54:06 +0000

    [Add]レビュー一覧から他のユーザーのマイページへ遷移できるように修正

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,7 +3,7 @@ class Public::UsersController < ApplicationController
   end
 
   def show
-    @user = current_user #ログインしているユーザーのみ
+    @user = User.find(params[:id]) #ログインしているユーザーのみ
     posted_book_ids = current_user.posts.pluck(:book_id) #pluckの引数にbook_idを指定し、id情報のみを取得する
     @books = Book.where(id: posted_book_ids).includes(:posts) #whereで７行目で取得したidで絞り込みをする。合わせてBookに紐づくPostを取得する
   end
