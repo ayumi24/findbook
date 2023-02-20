ef361bfbc92bc3da0e803ba72875dd4ff02830af
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2023-02-20 11:33:47 +0000

    [Add]他のユーザーページへ遷移できるようにコード記述変更

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -3,8 +3,8 @@ class Public::UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id]) #ログインしているユーザーのみ
-    posted_book_ids = current_user.posts.pluck(:book_id) #pluckの引数にbook_idを指定し、id情報のみを取得する
+    @user = User.find(params[:id]) 
+    posted_book_ids = @user.posts.pluck(:book_id) #pluckの引数にbook_idを指定し、id情報のみを取得する
     @books = Book.where(id: posted_book_ids).includes(:posts) #whereで７行目で取得したidで絞り込みをする。合わせてBookに紐づくPostを取得する
   end
 
