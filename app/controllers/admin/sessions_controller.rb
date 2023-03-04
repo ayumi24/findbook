29565c1cbdd5d10fc307e66d99f3ae33d44db330
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2023-03-04 07:46:45 +0000

    [Add]管理者ログイン画面レイアウト調整

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -5,6 +5,10 @@ class Admin::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     admin_homes_top_path(resource)
   end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
