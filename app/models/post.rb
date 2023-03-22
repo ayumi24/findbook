58c2059a76945b697a3d45fa9f04089854f3dea1
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2023-03-22 04:30:33 +0000

    [Add]バリデーション設定（要修正）

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,7 @@ class Post < ApplicationRecord
   belongs_to :book
   belongs_to :tag, optional: true
   has_many :post_comments, dependent: :destroy
+
+  validates :comment, presence: true
+  validates :readingtime, presence: true
 end
