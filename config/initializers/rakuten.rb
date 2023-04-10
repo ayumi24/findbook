2fd62d7f93c324d6d7990875169a40d8c3e86779
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2023-04-10 08:17:33 +0000

    [Add] APIkey記述修正

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,5 +1,5 @@
 RakutenWebService.configure do |c|
-  c.application_id = ENV['RWS_APPLICATION_ID']
+  c.application_id = '1046322930967945120'
 
-  c.affiliate_id = ENV['RWS_AFFILIATION_ID']
+  c.affiliate_id = '2f2c0bad.3a9a01c5.2f2c0bae.1136a917'
 end
