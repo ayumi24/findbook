3056eddd1d67f47d5b369f6145066a206a55e245
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2023-04-10 01:29:21 +0000

    [Add]コメント送信にバリデーション設定

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -6,8 +6,12 @@ class Public::PostCommentsController < ApplicationController
     post = Post.find(params[:post_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_id = post.id
-    comment.save
-    redirect_to public_post_path(post.id)
+     if comment.save
+       redirect_to public_post_path(post.id)
+     else
+       flash[:notice] = "コメントを入力して送信してください"
+       redirect_to public_post_path(post.id)
+     end
   end
 
 
