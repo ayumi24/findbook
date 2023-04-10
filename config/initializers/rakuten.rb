e31151d80e10e425d17a03334f4eafeafd503dbc
EC2 Default User <ec2-user@ip-172-31-36-246.ap-northeast-1.compute.internal>
2023-04-10 07:47:51 +0000

    [Add]楽天APIkey記述修正

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,5 +1,5 @@
 RakutenWebService.configure do |c|
-  #c.application_id = ''
+  c.application_id = ENV['RWS_APPLICATION_ID']
 
-  #c.affiliate_id = ''
+  c.affiliate_id = ENV['RWS_AFFILIATION_ID']
 end
